367059f12d3f8ce3513c57f3887272cb8f566e0a
Przemek Sech <przemek.sech@ansarada.com>
2019-12-14 23:12:05 +1100

    Working solution with x-api-key

--- serverless-static-website/src/backend/handler.js
+++ serverless-static-website/src/backend/handler.js
@@ -3,16 +3,25 @@
 const { authorize } = require("./auth-manager");
 const logger = require("./utils/logger");
 const prismportal = require("./prismportal-manager");
+const middy = require("middy");
+const { cors } = require("middy/middlewares");
 
-module.exports.getProducts = async event => {
+const getProducts = async event => {
   logger.log(">> handler.getProducts has been called");
 
   try {
     const authToken = await authorize();
     const availableProducts = await prismportal.getAvailableProducts(authToken);
+
+    // TODO: Consider filtering response to return limited json object 
+    // as full API response is not needed and may reveal some sensitive information.
+
     return {
       statusCode: 200,
-      headers: { "Access-Control-Allow-Origin": "*" },
+      // headers: {
+      //   "Access-Control-Allow-Origin": "*",
+      //   "Access-Control-Allow-Credentials": true
+      // },
       body: JSON.stringify(availableProducts)
     };
   } catch (error) {
@@ -20,3 +29,7 @@ module.exports.getProducts = async event => {
     return { statusCode: 500, body: JSON.stringify(error) };
   }
 };
+
+const getProductsHandler = middy(getProducts).use(cors());
+
+module.exports = { getProductsHandler };
